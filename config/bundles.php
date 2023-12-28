2116542febc2be74de6ba7fa564705b394d93a7b
moufida <moufida_klach@yahoo.fr>
2023-12-28 11:10:22 +0100

    mettre en place controller question et sidebar

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class QuestionController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
+    #[Route('/question/ask', name: 'question_form')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('question/index.html.twig', [
+            'controller_name' => 'QuestionController',
         ]);
     }
 }
